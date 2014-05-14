26a7af97a5a4b252ffeb07e5f9df6fbba9b64306
Rares Ambrus <raambrus@kth.se>
2014-05-14 16:37:07 +0200

    Managed to get the move_base_inflated action client working, goes to exact position of waypoint though

--- scripts/reconfigure_inflation_server.py
+++ scripts/reconfigure_inflation_server.py
@@ -14,8 +14,8 @@ class ReconfigureInflationServer(object):
         self.local_reconfig_client = dynamic_reconfigure.client.Client('/move_base/local_costmap/inflation_layer')
         self.global_reconfig_client = dynamic_reconfigure.client.Client('/move_base/global_costmap/inflation_layer')
         
-        self.feedback = BacktrackFeedback()
-        self.result = BacktrackResult()
+        #self.feedback = BacktrackFeedback()
+        #self.result = BacktrackResult()
         
         self.server = actionlib.SimpleActionServer('move_base_inflated', MoveBaseAction, self.execute, False)
         self.server.start()
@@ -29,10 +29,12 @@ class ReconfigureInflationServer(object):
     def execute(self, goal):    
         self.prev_local_inflation = rospy.get_param("/move_base/local_costmap/inflation_layer/inflation_radius")
         self.prev_global_inflation = rospy.get_param("/move_base/global_costmap/inflation_layer/inflation_radius")
-        local_params = { 'inflation_radius' : 1.8 }
-        global_params = { 'inflation_radius' : 1.8 }
+        local_params = { 'inflation_radius' : 0.8 }
+        global_params = { 'inflation_radius' : 0.8 }
         local_config = self.local_reconfig_client.update_configuration(local_params)
         global_config = self.local_reconfig_client.update_configuration(global_params)
+        
+        print "Set the inflation radius to 0.8"
         
         self.move_base_action_client.send_goal(goal)
         status = self.move_base_action_client.get_state()
@@ -41,15 +43,18 @@ class ReconfigureInflationServer(object):
             if self.server.is_preempt_requested():
                 self.reset_move_base_pars()
                 self.move_base_action_client.cancel_goal()
+                self.server.set_preempted(self.move_base_action_client.get_result())
                 return
             self.move_base_action_client.wait_for_result(rospy.Duration(0.2))
         
+        print "Reset the inflation radius to ", self.prev_local_inflation
+        
         self.reset_move_base_pars()
         if status == GoalStatus.ABORTED:
-            self.server.set_aborted(self.move_base_action_client.status())
+            self.server.set_aborted(self.move_base_action_client.get_result())
         if status == GoalStatus.PREEMPTED:
-            self.server.set_preempted(self.move_base_action_client.status())
-        self.server.set_succeded(self.move_base_action_client.status())
+            self.server.set_preempted(self.move_base_action_client.get_result())
+        self.server.set_succeeded(self.move_base_action_client.get_result())
 
 if __name__ == '__main__':
     server = ReconfigureInflationServer()
