54fba16c79a3eae2696a152cba7873fae5abc60a
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2014-05-30 11:05:24 +0100

    added printing for debug of cases where door pass stops wotking suddenly

--- src/door_pass.cpp
+++ src/door_pass.cpp
@@ -64,7 +64,7 @@ void poseCallback(const geometry_msgs::Pose::ConstPtr& msg)
         float closedDoorFinalAngle=1.550;
        // printf("DIST: %f\n", distToGoal);
 	if (state == TURNING){
-		
+		printf("IN STATE TURNING\n");
 		base_cmd.linear.x = 0; 
 		currentAngle = (atan2(rY,rX)-currentAngle);
 		while (currentAngle >= M_PI) currentAngle-= 2*M_PI;
@@ -73,59 +73,35 @@ void poseCallback(const geometry_msgs::Pose::ConstPtr& msg)
 		maxDistance = fmin(distToGoal,maxDistance);
 		if (fabs(currentAngle) < 0.1){
 			base_cmd.angular.z = 0;
+                        printf("FACING GOAL POSE, GOING TO STATE DETECT\n");
 			state = DETECT;
-		} 
+		}
+		printf("publishing to cmd_vel: base_cmd.linear.x = %f, base_cmd.angular.z= %f\n", base_cmd.linear.x, base_cmd.angular.z);
 		cmd_vel.publish(base_cmd);
 	}
 	if (state == DOOR_CLOSED){
+                printf("IN STATE DOOR_CLOSED\n");
                 base_cmd.linear.x = 0; 
                 currentAngle = (closedDoorFinalAngle-currentAngle);
                 while (currentAngle >= M_PI) currentAngle-= 2*M_PI;
                 while (currentAngle < -M_PI) currentAngle += 2*M_PI;
+                printf("TURNING TO PRE-DEFINED POSITION BEFORE ABORTING\n");
                 base_cmd.angular.z = currentAngle*0.5;
                 maxDistance = fmin(distToGoal,maxDistance);
                 if (fabs(currentAngle) < 0.1){
                         base_cmd.angular.z = 0;
                         path_pub.publish(empty_path);
+                        printf("REACHED PREDEFINED ORIENTATION?+> GOING TO STATE FAIL\n");
                         state = FAIL;
-                } 
+                }
+                printf("PUBLISHING TO CMD_VEL TO GET INTO PRE DEFINED ORIENTATION\n");
                 cmd_vel.publish(base_cmd);
         }
             
             
 }
 
-void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_msg)
-{       //state=PASS;
-// 	if (state == APPROACH || state == ADJUST || state == PASS || state == LEAVE){
-// 		base_cmd.linear.x = 0; 
-// 		base_cmd.angular.z = 0;
-// 		if (state == APPROACH || state == DETECT || state == ADJUST){
-// 			SDoor door = detectDoor(scan_msg->ranges,scan_msg->angle_min,scan_msg->angle_increment,scan_msg->ranges.size(),maxDistance);
-// 			if (door.found){
-// 				misdetections=0;
-// 				if (state == APPROACH){
-// 					if (debug) printf("Moving to %f %f ",door.auxX,door.auxY);
-// 					if (door.auxX < 0.05) passCounter++; else passCounter=0;
-// 					if (passCounter >  passCounterLimit){
-// 						state = ADJUST;
-// 						passCounter =0;
-// 					}
-// 					if (debug) printf("\n");
-// 					base_cmd.linear.x = fmax(fmin(defaultSpeed,door.auxX),0); 
-// 					base_cmd.angular.z = door.auxY; 
-// 					cmd_vel.publish(base_cmd);
-// 				}
-// 				if (state == ADJUST){
-// 					if (debug) printf("Turning %f \n",door.doorCentreY);
-// 					if (fabs(door.doorCentreY) < 0.05) passCounter++; else passCounter=0;
-// 					if (passCounter >  passCounterLimit) state = PASS;
-// 					base_cmd.linear.x = 0;
-// 					base_cmd.angular.z = door.doorCentreY; 
-// 					cmd_vel.publish(base_cmd);
-// 				}
-// 
-// 			}
+void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_msg) {    
                 float d;
                 float angle_min;
                 float angle_increment;
@@ -139,17 +115,15 @@ void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_msg)
                 int i;
                 
                 if (state == DETECT){
-                    
+                    printf("IN STATE DETECT\n");
                     angle_min = scan_msg->angle_min;
                     angle_increment =scan_msg->angle_increment;
+                    printf("CHECKING OPEN DOOR. DIST TO GOAL: %f\n", distToGoal);
                     for (i = 0; i <= num_ranges; i++) {
                         angle = angle_min+i*angle_increment;
                         d = scan_msg->ranges[i];
                         x = d*cos(angle)+0.07;
-                       
-                        if (angle> -0.26 && angle < 0.26) {
-                            printf("CHECKING OPEN DOOR. DIST TO GOAL: %f\n", distToGoal);
-                            printf("ITERATION: %d, FRONT DIST: %f\n", i, x);
+                        if (angle> -0.26 && angle < 0.26) {                            
                             if (x < distToGoal) {
                                 closedDoorCounter++;
                             } else {
@@ -160,13 +134,15 @@ void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_msg)
                     printf("CLOSED DOOR: %d\n", closedDoorCounter);
                     printf("OPEN DOOR: %d\n", openDoorCounter);
                     if (closedDoorCounter > openDoorCounter){
+                        printf("GOING TO STATE DOOR_CLOSED\n");
                         state=DOOR_CLOSED;
                     } else {
+                        printf("GOING TO STATE PASS\n");
 			state=PASS;
                     }
                 }
 		if (state == PASS){
-			
+			printf("IN STATE PASS\n");
 			leftMinim=rightMinim = 100;
 
 
@@ -185,24 +161,26 @@ void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_msg)
 			}
 			leftMinim-=baseRadius;
 			rightMinim-=baseRadius;
-			//if (debug) printf("Obstacles %f %f \n",leftMinim,rightMinim);
-			base_cmd.linear.x = fmax(fmin(3*fmin(rightMinim,leftMinim),defaultSpeed),0); 
+			printf("leftMinim: %f, rightMinin:  %f \n",leftMinim,rightMinim);
+			base_cmd.linear.x = fmax(fmin(3*fmin(rightMinim,leftMinim),defaultSpeed),0);
 			base_cmd.angular.z = 2*(leftMinim-rightMinim);
+                        printf("base_cmd.linear.x = %f, base_cmd.angular.z= %f\n", base_cmd.linear.x, base_cmd.angular.z);
 			if (leftMinim > 0.1 && rightMinim >0.1){
 				base_cmd.angular.z =0;
 				base_cmd.linear.x = 0.2;
-// 				passCounter++;
-// 			}else{
-// 				passCounter = -50;
+                                printf("leftMinim > 0.1 && rightMinim >0.1, updating: base_cmd.linear.x = %f, base_cmd.angular.z= %f\n", base_cmd.linear.x, base_cmd.angular.z);
 			}
 			//if (passCounter > passCounterLimit) state = LEAVE;
 			if (distToGoal<0.2){
+                            printf("close enough to goal pose, going to state LEAVE\n");
                             state=LEAVE;
                         }
                         if (distToGoal > 1.5){
+                            printf("too far from goal, going to state FAIL\n");
                             state=FAIL;
                         }
-			cmd_vel.publish(base_cmd);
+                        printf("publishing to cmd_vel: base_cmd.linear.x = %f, base_cmd.angular.z= %f\n", base_cmd.linear.x, base_cmd.angular.z);
+                        cmd_vel.publish(base_cmd);
 		}
 //	}
 }
@@ -212,9 +190,9 @@ void actionServerCallback(const move_base_msgs::MoveBaseGoalConstPtr& goal, Serv
 	move_base_msgs::MoveBaseResult result;
 	goalX = goal->target_pose.pose.position.x;
 	goalY = goal->target_pose.pose.position.y;
-	//misdetections = 0;
+        printf("ACTION SERVER CALLBACK JUST STARTED. GOAL is x=%f, y=%f\n", goalX, goalY); 
+	printf("GOING TO STATE TURNING\n");
 	state = TURNING;
-	if (goalX == 0 && goalY == 0) state = APPROACH;
 	while (state == TURNING || state == DETECT || state == DOOR_CLOSED||  state == PASS || state == LEAVE){
 		if (misdetections > maxMisdetections || state == LEAVE){
 			if (state == LEAVE) state = SUCCESS; else state = FAIL;
@@ -224,13 +202,14 @@ void actionServerCallback(const move_base_msgs::MoveBaseGoalConstPtr& goal, Serv
 	if (state == SUCCESS) server->setSucceeded(result);
 	if (state == FAIL) server->setAborted(result);
 	if (state == PREEMPTED) server->setPreempted(result);
+        printf("ACTION FINISHED, GOING TO STATE STOPPING\n");
 	state = STOPPING;
 }
 
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "doorPassing");
+	ros::init(argc, argv, "door_pass_node");
 	ros::NodeHandle n;
 	cmd_vel = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
         path_pub=n.advertise<nav_msgs::Path>("/move_base/NavfnROS/plan",1);
@@ -241,9 +220,11 @@ int main(int argc, char** argv)
 	while (ros::ok()){
 		if (server->isPreemptRequested() && state != IDLE) state = PREEMPTED; 
 		if (state == STOPPING)
-		{
+		{       
+                        printf("IN STATE STOPPING\n");
 			base_cmd.linear.x = base_cmd.angular.z = 0;
 			cmd_vel.publish(base_cmd);
+                        printf("PUBLISHED 0 VEL COMMANDS, GOING TO STATE IDLE\n");
 			state = IDLE;
 		} 
 		ros::spinOnce();
