66fff064e8733fbd57092dd4c29d746e76c6513a
Jaime Pulido Fentanes <pulidofentanes@gmail.com>
2014-11-20 14:54:35 +0000

    making strands_email less linda specific

--- strands_emails/script/send_emails.py
+++ strands_emails/script/send_emails.py
@@ -16,9 +16,13 @@ class emailServer(object):
     _result   = strands_emails.msg.SendEmailResult()
 
     def __init__(self, name):
-
         self.cancelled = False
         self._action_name = name
+
+        self.from_add = rospy.get_param("~from_add",'robot@strands.eu')
+        self.smtp_add = rospy.get_param("~smtp_add",'localhost:25')
+
+
         rospy.loginfo("Creating action server.")
         self._as = actionlib.SimpleActionServer(self._action_name, strands_emails.msg.SendEmailAction, execute_cb = self.executeCallback, auto_start = False)
         self._as.register_preempt_callback(self.preemptCallback)
@@ -31,16 +35,16 @@ class emailServer(object):
 
     def _send_email(self, goal):
         self.cancelled = False
-        me = 'linda@lincoln.ac.uk'
+        me = self.from_add
         msg = MIMEMultipart('alternative')
         msg['Subject'] = goal.subject
         msg['From'] = me
         msg['To'] = goal.to_address
-        texts = "Hi,\n%s\nCheers,\nLinda"%goal.text
+        texts = "%s"%goal.text
         part1 = MIMEText(texts, 'plain')
         msg.attach(part1)
         
-        server = smtplib.SMTP('localhost:25')
+        server = smtplib.SMTP(self.smtp_add)
         
         server.sendmail(me, goal.to_address, msg.as_string())
         server.quit()
