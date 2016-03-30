ba5b1effcda06d8f47ea909e5d236174388deb13
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2016-03-30 18:44:42 +0100

    log waits to mongo

--- door_pass/src/door_pass/door_utils.py
+++ door_pass/src/door_pass/door_utils.py
@@ -5,7 +5,7 @@ from std_msgs.msg import String
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist, Pose
 from nav_msgs.msg import Path
-from door_pass.msg import DoorCheckStat
+from door_pass.msg import DoorCheckStat, DoorWaitStat
 from mongodb_store.message_store import MessageStoreProxy
 from actionlib import SimpleActionClient
 from mary_tts.msg import maryttsAction, maryttsGoal
@@ -47,8 +47,7 @@ class DoorUtils(object):
         self.new_scan_msg = False
         
         self.is_active=False
-        self.is_waiting=False
-        self.mongo_logger=message_proxy=MessageStoreProxy(collection='door_checks')
+        self.mongo_logger=message_proxy=MessageStoreProxy(collection='door_stats')
         self.speaker = SimpleActionClient('/speak', maryttsAction)
         
     def activate(self):
@@ -153,11 +152,9 @@ class DoorUtils(object):
             return False
     
     def wait_door(self, wait_timeout, target_pose=None, n_closed=10, log_to_mongo=True, speak=True,consecutive_opens=4):
-        self.is_waiting=True
         open_count=0
-        
-        wait_timer=rospy.Timer(rospy.Duration(wait_timeout), self.wait_timer_cb, oneshot=True)
-        while self.is_active and self.is_waiting and open_count<consecutive_opens:
+        wait_elapsed=0.0
+        while self.is_active and wait_elapsed < wait_timeout and open_count<=consecutive_opens:
             rospy.loginfo("Door wait and pass action server calling check door")
             door_open=self.check_door(target_pose, n_closed, False)
             if door_open:
@@ -167,21 +164,25 @@ class DoorUtils(object):
             if speak and open_count==1:
                 self.speaker.send_goal(maryttsGoal(text="Please hold the door!"))
             rospy.sleep(rospy.Duration(0.5))
-        wait_timer.shutdown()
-        self.is_waiting=False
+            wait_elapsed+=0.5
         
         if not self.is_active:
             return False
       
         opened=(open_count==consecutive_opens)
         if log_to_mongo:
-            print("LGOGLOGLGOLGOOLOG")
+            try:
+                waypoint=rospy.wait_for_message("/current_node", String, 5)
+                topological_map_name=rospy.get_param("/topological_map_name", "")
+                self.mongo_logger.insert(DoorWaitStat(topological_map_name=topological_map_name,
+                                                    waypoint=waypoint.data,
+                                                    opened=opened,
+                                                    wait_time=wait_elapsed))
+            except Exception, e:
+                rospy.logwarn("Error logging door check " + str(e))
         return opened
         
-        
-    def wait_timer_cb(self, event):
-        self.is_waiting=False    
-        
+
     
     def pass_door(self, target_pose, speech=False): #assumes robot is facing the door and the door is open
         if self.is_active:
